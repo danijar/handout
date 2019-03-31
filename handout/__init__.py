be37ac83d318ad45cbb09aedff09bf86004f2076
Danijar Hafner <mail@danijar.com>
2019-03-31 15:47:47 -0400

    Avoid logging messages twice.

--- handout/__init__.py
+++ handout/__init__.py
@@ -7,4 +7,7 @@ from .handout import Handout
 # Set up logger.
 logger = logging.getLogger('handout')
 logger.setLevel(logging.INFO)
-logger.addHandler(logging.StreamHandler(sys.stdout))
+logger.propagate = False  # Global logger should not print messages again.
+handler = logging.StreamHandler(sys.stdout)
+handler.setFormatter(logging.Formatter('%(message)s'))
+logger.addHandler(handler)
